c83b4c4d99ddce9d2d4b3feca6c47d69ff474b22
KryPat <patryk14901@gmail.com>
2023-01-23 12:45:38 +0100

    added counter of tries

--- src/HiLo.java
+++ src/HiLo.java
@@ -9,21 +9,28 @@ public class HiLo {
         do {
 
             // Tworzenie liczby losowej do odgadnięcia przez użytkownika
-            int theNumber = (int) (Math.random() * 100 + 1);
-            // System.out.println(theNumber);
+            int theNumber = (int) ((Math.random() * 200 + 1) - 100);
+            System.out.println(theNumber);
             int guess = 0;
+            int numberOfTries = 1;
             while (guess != theNumber) {
                 // Petla while
                 // Prosi urzytkownika o wpsianie liczby, wyświetla okienko
-                System.out.println("Guess a number between 1 and 100:");
+                System.out.println("Guess a number between -100 and 100:");
                 guess = scan.nextInt();
                 System.out.println("You entered: " + guess + ".");
-                if (guess < theNumber)
+                if (guess < theNumber) {
                     System.out.println(guess + " is too low. Try again.");
-                else if (guess > theNumber)
+                    numberOfTries += 1;
+                }
+                else if (guess > theNumber) {
                     System.out.println(guess + " is too high. Try again.");
-                else
+                    numberOfTries += 1;
+                }
+                else {
                     System.out.println(guess + " is correct. You Win!");
+                    System.out.println("Number of tries is: " + numberOfTries);
+                }
 
             } // Zakończenie petli while do zgadywania
                 System.out.println("would you like to play again? (y/n)");
